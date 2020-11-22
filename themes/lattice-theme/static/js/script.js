6a6e7b513257a56fcd2c4b04fc57288f88020d4a
Moby von Briesen <mobyvb@gmail.com>
2020-11-21 19:12:01 -0500

    Add sun/moon icons for night mode, add header partial, and remember day vs. night mode on reload.

--- themes/lattice-theme/static/js/script.js
+++ themes/lattice-theme/static/js/script.js
@@ -1,40 +1,27 @@
-console.log("hello")
-
-let fg = document.getElementById("fg");
-let bg = document.getElementById("bg");
-
-document.addEventListener("scroll", function(e) {
-    requestAnimationFrame(function(e) {
-        /*
-        fg.style.transform = "translate3d("+window.scrollY+"px, -62px, -135px);"
-        bg.style.transform = "translate3d(42px, -62px, -135px);"
-        */
-        let scrollAmount = window.scrollY * 0.1;
-        //bg.style.transform="translate3d(0, "+scrollAmount+"px, 0)"
-
-    })
+window.addEventListener('load', (event) => {
+    if (localStorage["night"] === true) {
+        setNight()
+    }
 })
 
+var body = document.body
+
 function toggleNight() {
-    body = document.body
     if (body.classList.contains("night")) {
-        body.classList.remove("night")
-        body.classList.add("day")
+        setDay()
     } else {
-        body.classList.remove("day")
-        body.classList.add("night")
+        setNight()
     }
-        /*
-    if (fg.classList.contains("night")) {
-        fg.classList.remove("night")
-        bg.classList.remove("night")
-        fg.classList.add("day")
-        bg.classList.add("day")
-    } else {
-        fg.classList.remove("day")
-        bg.classList.remove("day")
-        fg.classList.add("night")
-        bg.classList.add("night")
-    }
-    */
+}
+
+function setNight() {
+    body.classList.remove("day")
+    body.classList.add("night")
+    localStorage["night"] = true
+}
+
+function setDay() {
+    body.classList.remove("night")
+    body.classList.add("day")
+    localStorage["night"] = false
 }
